b984e61e87fd5339f9c0b8a06048853549f292c9
Xing Yang <xingyang105@gmail.com>
2019-08-24 05:13:19 -0700

    Make changes to types.go based the latest KEP
    TODO: Need to change controller logic accordingly to use
    the changed types.go.

--- pkg/apis/executionhook/v1alpha1/types.go
+++ pkg/apis/executionhook/v1alpha1/types.go
@@ -23,55 +23,44 @@ import (
 )
 
 const (
-        // ExecutionHookTemplateResourcePlural is "executionhooktemplates"
-        ExecutionHookTemplateResourcePlural = "executionhooktemplates"
-        // ExecutionHookResourcePlural is "executionhooks"
-        ExecutionHookResourcePlural = "executionhooks"
+	// HookActionResourcePlural is "hookactions"
+	HookActionResourcePlural = "hookactions"
+	// ExecutionHookResourcePlural is "executionhooks"
+	ExecutionHookResourcePlural = "executionhooks"
 )
 
 // ExecutionHookSpec defines the desired state of ExecutionHook
+// HookActionName is copied to ExecutionHookSpec by the controller such as
+// the Snapshot Controller.
 type ExecutionHookSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// Without PodSelector/ContainerSelector, Snapshot controller will automatically detect
-	// pods/containers needed to run the hook from the volume source of the snapshot.
-	// If PodSelector/ContainerSelector are specified in ExecutionHookTemplate, snapshot
-	// controller will copy them to ExecutionHookSpec. ExecutionHook controller will find all
-	// pods and containers based on PodSelector and containerSelector.
+	// PodSelection defines how to select pods and containers to run
+	// the executionhook. If multiple pod/containers are selected, the action will executed on them
+	// asynchronously. If execution ordering is required, caller has to implement the logic and create
+	// different hooks in order.
+	// This field is required.
+	PodSelection PodSelection `json:"podSelection" protobuf:"bytes,1,opt,name=podSelection"`
+
+	// Name of the HookAction. This is required.
+	ActionName string `json:"actionName" protobuf:"bytes,2,opt,name=actionName"`
+}
+
+// PodSelection contains two fields, PodContainerNamesList and PodContainerSelector,
+// where one of them must be defined so that the hook controller knows where to
+// run the hook.
+type PodSelection struct {
+	// PodContainerNamesList lists the pods/containers on which the ExecutionHook
+	// should be executed. If not specified, the ExecutionHook controller will find
+	// all pods and containers based on PodContainerSelector.
+	// If both PodContainerNamesList and PodContainerSelector are not
+	// specified, the ExecutionHook cannot be executed and it will fail.
 	// +optional
 	PodContainerNamesList []PodContainerNames `json:"podContainerNamesList,omitempty" protobuf:"bytes,1,rep,name=podContainerNamesList"`
 
-	// PodSelector is a selector which must be true for the hook to run on the pod
-	// Hook controller will find all pods based on PodSelector, if specified
-	// This can either be copied from the ExecutionHookTemplate by the controller or
-	// new PodSelector can be created in the spec to override the one in the template
+	// PodContainerSelector is for hook controller to find pods and containers
+	// based on the pod label selector and container names
+	// If PodContainerNamesList is specified, this field will not be used.
 	// +optional
-	PodSelector *metav1.LabelSelector `json:"podSelector,omitempty" protobuf:"bytes,2,opt,name=podSelector"`
-
-	// ContainerSelector is a selector which must be true for the hook to run on the container
-	// Hook controller will find all containers in the selected pods based on ContainerSelector,
-	// if specified
-	// This can either be copied from the ExecutionHookTemplate by the controller or
-	// new ContainerSelector can be created in the spec to override the one in the template
-	// If ContainerSeletor is not specified, the first container on the selected pod will be
-	// chosen by default.
-	// +optional
-	ContainerSelector map[string]string `json:"containerSelector,omitempty" protobuf:"bytes,3,rep,name=containerSelector"`
-
-	// Policy is not needed in the spec. If ExecuteOnce, only one pod name will be added here.
-	// If ExecuteAll, all pod names will be added here.
-	// This field is required.
-	PreAction *ExecutionHookAction `json:"preAction" protobuf:"bytes,4,opt,name=preAction"`
-
-	// +optional
-	PostAction *ExecutionHookAction `json:"postAction,omitempty" protobuf:"bytes,5,opt,name=postAction"`
-
-	// Time between PreAction and PostAction
-	// ExecutionHook controller will check this timeout. If API server dies, hook controller will still trigger
-	// PostAction command if this field is set, timeout happens, and PreAction is called (there is a
-	// PreActionStartTimestamp)
-	// +optional
-	ExpirationSeconds *int64 `json:"expirationSeconds,omitempty" protobuf:"varint,6,opt,name=expirationSeconds"`
+	PodContainerSelector *PodContainerSelector `json:"podContainerSelector,omitempty" protobuf:"bytes,2,opt,name=podContainerSelector"`
 }
 
 type PodContainerNames struct {
@@ -82,6 +71,17 @@ type PodContainerNames struct {
 	ContainerNames []string `json:"containerNames,omitempty" protobuf:"bytes,2,rep,name=containerNames"`
 }
 
+type PodContainerSelector struct {
+	// PodSelector specifies a label query over a set of pods.
+	// +optional
+	PodSelector *metav1.LabelSelector `json:"podSelector,omitempty" protobuf:"bytes,1,opt,name=podSelector"`
+
+	// If specified, controller only select the containers that are listed from the selected pods based on PodSelector.
+	// Otherwise, all containers of the pods will be selected
+	// +optional
+	ContainerList []string `json:"containerList,omitempty" protobuf:"bytes,2,rep,name=containerList"`
+}
+
 type ExecutionHookAction struct {
 	// This field is required.
 	Action *core_v1.Handler `json:"action" protobuf:"bytes,1,opt,name=action"`
@@ -90,23 +90,13 @@ type ExecutionHookAction struct {
 	ActionTimeoutSeconds *int64 `json:"actionTimeoutSeconds,omitempty" protobuf:"bytes,2,opt,name=actionTimeoutSeconds"`
 }
 
-// ExecutionHookStatus defines the observed state of ExecutionHook
+// ExecutionHookStatus represents the current state of a hook
 type ExecutionHookStatus struct {
 	// This is a list of ContainerExecutionHookStatus, with each status representing
-	// information about how hook is executed in a pod, including pod name, container name,
-	// preaction timestamp, preaction succeed, etc.)
+	// information about how hook is executed in a container, including pod name,
+	// container name, ActionTimestamp, ActionSucceed, etc.
 	// +optional
-	ContainerExecutionHookStatuses []ContainerExecutionHookStatus `json:"containerExecutionHookStatuses,omitempty" protobuf:"bytes,1,rep,name=containerExecutionHookStatuses"`
-
-	// PreAction Summary status
-	// +optional
-	// Default is nil
-	PreActionSucceed *bool `json:"preActionSucceed,omitempty" protobuf:"varint,2,opt,name=preActionSucceed"`
-
-	// PostAction Summary status
-	// +optional
-	// Default is nil
-	PostActionSucceed *bool `json:"postActionSucceed,omitempty" protobuf:"varint,3,opt,name=postActionSucceed"`
+	HookStatuses []ContainerExecutionHookStatus `json:"containerExecutionHookStatuses,omitempty" protobuf:"bytes,1,rep,name=containerExecutionHookStatuses"`
 }
 
 // ContainerExecutionHookStatus represents the current state of a hook for a specific
@@ -118,51 +108,68 @@ type ContainerExecutionHookStatus struct {
 	// This field is required
 	ContainerName string `json:"containerName" protobuf:"bytes,2,opt,name=containerName"`
 
-	// This represents the status of a PreAction on a container in a pod
-	// +optional
-	PreActionStatus *ExecutionHookActionStatus `json:"preActionStatus,omitempty" protobuf:"bytes,3,opt,name=preActionStatus"`
-
-	// This represents the status of a PostAction on a container in a pod
-	// +optional
-	PostActionStatus *ExecutionHookActionStatus `json:"postActionStatus,omitempty" protobuf:"bytes,4,opt,name=postActionStatus"`
-}
-
-// The current state of an action on one container in a pod
-type ExecutionHookActionStatus struct {
 	// If not set, it is nil, indicating Action has not started
 	// If set, it means Action has started at the specified time
 	// +optional
-	ActionTimestamp *metav1.Time `json:"actionTimestamp,omitempty" protobuf:"bytes,1,opt,name=actionTimestamp"`
+	Timestamp *metav1.Time `json:"actionTimestamp,omitempty" protobuf:"bytes,1,opt,name=actionTimestamp"`
 
-	// If not set, it is nil, indicating Action is not complete on the specified pod
+	// Succeed is set to true when the action is executed in the container successfully.
+	// It will be set to false if the action cannot be executed successfully after
+	// ActionTimeoutSeconds passes.
 	// +optional
-	ActionSucceed *bool `json:"actionSucceed,omitempty" protobuf:"varint,2,opt,name=actionSucceed"`
+	Succeed *bool `json:"actionSucceed,omitempty" protobuf:"varint,2,opt,name=actionSucceed"`
 
-	// The last error encountered when running the action
+	// The last error encountered when executing the action. The hook controller might
+	// update this field each time it retries the execution.
 	// +optional
-	Error *storage.VolumeError `json:"error,omitempty" protobuf:"bytes,3,opt,name=error"`
+	Error *HookError `json:"error,omitempty" protobuf:"bytes,3,opt,name=error"`
 }
 
-type HookPolicy string
+type HookError struct {
+	// Type of the error
+	// This is required
+	ErrorType ErrorType
 
-// These are valid policies of ExecutionHook
+	// Error message
+	// +optional
+	Message *string
+
+	// More detailed reason why error happens
+	// +optional
+	Reason *string
+
+	// It indicates when the error occurred
+	// +optional
+	Timestamp *metav1.Time
+}
+
+type ErrorType string
+
+// More error types could be added, e.g., Forbidden, Unauthorized, AlreadyInProgress, etc.
 const (
-	// Execute commands only once on any one selected pod
-	HookExecuteOnce HookPolicy = "ExecuteOnce"
-	// Execute commands on all selected pods
-	HookExecuteAll HookPolicy = "ExecuteAll"
+	// The execution hook times out
+	Timeout ErrorType = "Timeout"
+
+	// The execution hook fails with an error
+	Error ErrorType = "Error"
 )
 
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // ExecutionHook is the Schema for the executionhooks API
+// ExecutionHook is in the tenant namespace
 // +k8s:openapi-gen=true
 type ExecutionHook struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   ExecutionHookSpec   `json:"spec,omitempty"`
+	// Spec defines the behavior of a hook.
+	// +optional
+	Spec ExecutionHookSpec `json:"spec,omitempty"`
+
+	// Status defines the current state of a hook.
+	// +optional
 	Status ExecutionHookStatus `json:"status,omitempty"`
 }
 
@@ -178,70 +185,48 @@ type ExecutionHookList struct {
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// ExecutionHookTemplate describes pre and post action commands to run on selected pods based
-// on specified policies. If PodSelector/ContainerSelector are not specified, snapshot controller will
-// figure out pods/containers based on the volume source of the snapshot. If specified, the selectors
-// will be copied to the ExecutionHookSpec and the hook controller will find out what pods/containers
-// should run the commands. Note that selectors in ExecutionHookSpec can override those in template.
-// It is namespaced.
-// ExecutionHookTemplate is the Schema for the executionhooktemplates API.
+// HookAction describes action commands to run on pods/containers based
+// on specified policies. HookAction will be created by the user and
+// can be re-used later. Snapshot Controller will create ExecutionHooks
+// based on HookActions specified in the snapshot spec. For example,
+// two HookActions, preSnapshotExecutionHook and postSnapshotExecutionHook,
+// are expected in the snapshot spec.
+// HookAction does not contain information on pods/containers because those are
+// runtime info.
+// HookAction is namespaced.
 // +k8s:openapi-gen=true
-type ExecutionHookTemplate struct {
+type HookAction struct {
 	metav1.TypeMeta `json:",inline"`
 
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 
-	// Policy specifies whether to execute once on one selected pod or execute on all selected pods
-	// +optional
-	Policy *HookPolicy `json:"hookPolicy,omitempty" protobuf:"bytes,2,opt,name=hookPolicy"`
+	// This contains the command to run on a container.
+	// The command should be idempotent because the system does not guarantee exactly-once semantics.
+	// Any action may be triggered more than once but only the latest results will be logged in status.
+	// As alpha feature, only ExecAction type in Handler will be support, not the HTTPGETAction or TCPSocketAction.
+	// This is required.
+	Action core_v1.Handler `json:"action" protobuf:"bytes,2,opt,name=action"`
 
-	// This field is required
-	PreAction *ExecutionHookAction `json:"preAction" protobuf:"bytes,3,opt,name=preAction"`
-
-	// PostAction is optional
+	// ActionTimeoutSeconds defines when the execution hook controller should stop retrying.
+	// If execution fails, the execution hook controller will keep retrying until reaching
+	// ActionTimeoutSeconds. If execution still fails or hangs, execution hook controller
+	// stops retrying and updates executionhook status to failed.
+	// If controller loses its state, counter restarts. In this case, controller will retry
+	// for at least this long, before stopping.
+	// Once an action is started, controller has no way to stop it even if
+	// ActionTimeoutSeconds is exceeded. This simply controls if retry happens or not.
+	// retry is based on exponential backoff policy. If ActionTimeoutSeconds is not
+	// specified, it will retry until the hook object is deleted.
 	// +optional
-	PostAction *ExecutionHookAction `json:"postAction" protobuf:"bytes,4,opt,name=postAction"`
-
-	// Time between PreAction and PostAction
-	// ExecutionHook controller will check this timeout. If API server dies, hook controller will still trigger
-	// PostAction command if this field is set, timeout happens, and PreAction is called (there is a
-	// PreActionStartTimestamp)
-	// +optional
-	ExpirationSeconds *int64 `json:"expirationSeconds,omitempty" protobuf:"varint,5,opt,name=expirationSeconds"`
+	ActionTimeoutSeconds *int64 `json:"actionTimeoutSeconds,omitempty" protobuf:"varint,3,opt,name=actionTimeoutSeconds"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// ExecutionHookTemplateList contains a list of ExecutionHookTemplate
-type ExecutionHookTemplateList struct {
-        metav1.TypeMeta `json:",inline"`
-        metav1.ListMeta `json:"metadata,omitempty"`
-        Items           []ExecutionHookTemplate `json:"items"`
-}
-
-type ExecutionHookInfo struct {
-        // PodSelector is a selector which must be true for the hook to run on the pod
-        // PodSelector in ExecutionHook can override the selector in template
-        // Hook controller will find all pods based on PodSelector
-        // +optional
-        PodSelector *metav1.LabelSelector `json:"podSelector,omitempty" protobuf:"bytes,1,opt,name=podSelector"`
-
-        // ContainerSelector is a selector which must be true for the hook to run on the container
-        // ContainerSelector in ExecutionHook can override the selector in template
-        // Hook controller will find all containers in the selected pods based on ContainerSelector
-        // +optional
-        ContainerSelector map[string]string `json:"containerSelector,omitempty" protobuf:"bytes,2,rep,name=containerSelector"`
-
-        // Without PodSelector/ContainerSelector, Snapshot controller will automatically detect
-        // pods/containers needed to run the hook from the volume source of the snapshot.
-        // If PodSelector/ContainerSelector are specified in ExecutionHookTemplate, snapshot
-        // controller will copy them to ExecutionHookSpec. ExecutionHook controller will find all
-        // pods and containers based on PodSelector and containerSelector.
-        // +optional
-        PodContainerNamesList []PodContainerNames `json:"podContainerNamesList,omitempty" protobuf:"bytes,3,rep,name=podContainerNamesList"`
-
-        // Name of the ExecutionHookTemplate
-        // This is required and must have a valid hook template name; an empty string is not valid
-	ExecutionHookTemplateName string `json:"executionHookTemplateName",omitempty" protobuf:"bytes,4,rep,name=executionHookTemplateName"`
+// HookActionList contains a list of HookAction
+type HookActionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HookAction `json:"items"`
 }
